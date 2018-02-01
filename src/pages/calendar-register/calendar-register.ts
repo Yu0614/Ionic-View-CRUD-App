d665e1cbf3a6c8ca152f2090329866a42aca2e75
Yu0614 <azu369yu@gmail.com>
2018-02-01 10:49:05 +0900

    This is roop function test

--- src/pages/calendar-register/calendar-register.ts
+++ src/pages/calendar-register/calendar-register.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import firebase from 'firebase';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
 //import { Observable } from 'rxjs/Observable';
 import { AngularFireDatabase} from "angularfire2/database";
 //import { AngularFireObject } from 'angularfire2/database/interfaces';
@@ -12,18 +12,37 @@ import { ChartPage } from '../chart/chart';
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
  */
-
-@IonicPage()
+ 
 @Component({
   selector: 'page-calendar-register',
   templateUrl:'calendar-register.html',
-  
+  /*template: `
+  <ion-item *ngFor="let ref of roopReffer">
+          <ion-row>
+              <ion-col>
+                <div class="col" id="RefEventDate">{{ref.EventDate}}</div>
+              </ion-col>
+              <ion-col>  
+                <div class="col" id="RefCoinName">{{ref.CoinName}}</div>
+              </ion-col>
+              <ion-col>
+                <div class="col" id="RefImportance">{{ref.Importance}}</div>
+              </ion-col>
+              <ion-col>
+                  <div class="col" id="RefEventName">{{ref.EventName}}</div>
+              </ion-col>
+              <ion-col>
+                  <div class="col" id="RefArticleURL">{{ref.ArticleURL}}</div>
+              </ion-col>  
+          </ion-row>
+        </ion-item>
+        `,*/
 })
 export class CalendarRegisterPage {
 
     reffer; 
     DatabaseViewURL;
-    roopReffer;
+    roopReffer; // htmlに受け渡しする箱
 
   constructor(
     public navCtrl: NavController, 
@@ -33,10 +52,15 @@ export class CalendarRegisterPage {
   ) {
       this.reffer = {}; 
       this.DatabaseViewURL='Calendar/2018-01-30/Bit'; // Default table DB URL set
-      this.roopReffer= [
+      /*this.roopReffer= [//　箱のひな形？　
         {
-          
-      }]
+          ArticleURL:'' ,
+          CoinName:'' ,
+          EventDate: '',
+          EventName: '' ,
+          Importance: ''
+        }
+      ] */
 
       
     }
@@ -96,31 +120,35 @@ export class CalendarRegisterPage {
   * ★ delete Event (Notice: the Order is important)
   *************************************************/
  
-  /*deleteEvent(): void{
+  deleteEvent(): void{
     const deleteRef: firebase.database.Reference = firebase.database().ref(`/Calendar/2018_01_30/Bit/`);
     deleteRef.remove()
-  }*/
+  }
+
+
 
   /*************************************************
   * ★ referrence Function
   *************************************************/
 
-  
+  //Default set table
   ionViewDidLoad() :void{
     const readRef: firebase.database.Reference = firebase.database().ref(this.DatabaseViewURL);
-    
     readRef.on('value', readSnapshot => {
        this.reffer = readSnapshot.val();
     }); 
   }
 
+
+  // search and show Table from database
   searchTable(DatabaseViewRequest: string) :void{
     this.DatabaseViewURL = DatabaseViewRequest;
     const readRef: firebase.database.Reference = firebase.database().ref(this.DatabaseViewURL);
     
     readRef.on('value', readSnapshot => {
-       this.reffer = readSnapshot.val();
+       this.roopReffer = readSnapshot.val();
     }); 
+
   } 
   
   /*************************************************
